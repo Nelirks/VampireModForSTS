824cf1aa9ba1a13f19a55dc979d7b292efc35d5d
Grunlus <arlaudcorentin2305@gmail.com>
2022-03-08 19:35:39 +0100

    added CursedFumes and fixed Devour animation

---
+++ theDefault/src/main/java/theVampire/cards/CursedFumes.java
@@ -0,0 +1,78 @@
+package theVampire.cards;
+
+import basemod.AutoAdd;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.PoisonPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import theVampire.DefaultMod;
+import theVampire.characters.TheDefault;
+
+import java.util.Iterator;
+
+import static theVampire.DefaultMod.makeCardPath;
+
+public class CursedFumes extends AbstractDynamicCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = DefaultMod.makeID(CursedFumes.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Attack.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 2;
+    private static final int UPGRADED_COST = 1;
+
+    // /STAT DECLARATION/
+
+
+    public CursedFumes() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!p.hasPower("theVampire:Thirst")) {
+            return;
+        }
+        if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
+            this.flash();
+
+            for (AbstractMonster monster : AbstractDungeon.getMonsters().monsters) {
+                if (!monster.isDead && !monster.isDying) {
+                    this.addToBot(new ApplyPowerAction(monster, p, new VulnerablePower(monster, p.getPower("theVampire:Thirst").amount, false)));
+                }
+            }
+        }
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
